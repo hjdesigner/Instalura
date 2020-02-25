6767d8b3139a59d0145b88f42a0f1f12910cf169
Henrique Melanda <henrique.melanda@vagas.com.br>
2020-02-25 15:57:28 -0300

    Efetuando login e Navegando entre telas

--- App.js
+++ App.js
@@ -1,51 +1,20 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import {
-  FlatList,
-  StatusBar,
-  View,
-  Platform,
-} from "react-native";
-import { Cabecalho } from './src/Components/Cabecalho';
-import { Foto } from './src/Components/Foto';
-import {Comentarios} from './src/Components/Comentarios'
-import lerFotos from './src/api/feed';
+import Feed from "./src/Views/Feed/Feed"
+import Login from "./src/Views/Login/Login"
+import { createStackNavigator } from "react-navigation-stack"
+import { createAppContainer } from "react-navigation"
 
+const navigator = createStackNavigator({
+  Login :{ screen: Login },
+  Feed : { screen: Feed}
+})
+
+const AppContainer = createAppContainer(navigator)
 
 const App = () => {
-  const [fotos, setFotos] = useState([]);
-
-  useEffect(() => {
-    lerFotos(setFotos);
-  }, []);
-
-  let altura = 0;
-  if (Platform.OS == "ios") {
-    altura = 35;
-  }
-
+  
   return (
-    <View style={{ marginTop: altura }}>
-      <StatusBar
-        backgroundColor="white"
-        barStyle="dark-content"
-      />
-      <FlatList
-        data={fotos}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) =>
-          <Fragment>
-            <Cabecalho
-              nomeUsuario={item.userName}
-              urlImage={item.userURL} />
-            <Foto
-              urlFoto={item.url}
-              descricao={item.description}
-              qntLikes={item.likes}
-            />
-            <Comentarios comentarios={item.comentarios}/>
-          </Fragment>}
-      />
-    </View>
+    <AppContainer/>
   )
 };
 export default App;
---
+++ src/Views/Login/Login.js
@@ -0,0 +1,58 @@
+import React, {Fragment , useState} from 'react';
+import {
+  Text,
+  TextInput,
+  Button,
+  View
+
+} from "react-native";
+import AsyncStorage from "@react-native-community/async-storage"
+import estilo from "./estilo"
+import efetuarLogin from '../../api/login';
+
+const Login = ({ navigation }) => {
+const [usuario, setUsuario] = useState("");
+const [senha, setSenha] = useState("");
+const [mensagemErro, setMensagemErro] = useState("")
+
+  const tentarLogar = async ()=> {
+    try{
+      const token = await efetuarLogin(usuario, senha);
+      await AsyncStorage.setItem("instalura_token", token);
+      //Ir para tela de Feed
+      navigation.push("Feed")
+
+    }catch(erro){
+      setMensagemErro(erro.message)
+    }
+   
+  }
+  
+  return (
+    <Fragment>
+      <View style={estilo.container}>
+        <TextInput 
+        style={estilo.inputs}
+         placeholder="Usuário"
+         onChangeText={texto => setUsuario(texto)}
+         />
+         <TextInput 
+         style={estilo.inputs}
+         placeholder="Senha"
+         secureTextEntry={true}
+         onChangeText={texto => setSenha(texto)}
+         />
+         <Text>{mensagemErro}</Text>
+      </View>
+
+      <View style={estilo.botaoView}>
+        <Button title="Entrar" onPress={tentarLogar}/>
+      </View>
+      
+
+    </Fragment>
+    
+
+  )
+};
+export default Login;
